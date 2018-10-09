98fb11ee16cc5441cc8d9808bf551222e3914d7d
Angel Castro <al.castro@gmail.com>
2018-10-09 18:48:41 +0200

    Added a local and a global component template.

---
+++ vue/vue-framework-intro/src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="user">
+        <h3>The user</h3>
+        <p>My username is {{username}}</p>
+        <app-info></app-info>
+    </div>
+</template>
+<script>
+    import Info from "./Info.vue";
+    export default {
+        data() {
+            return { username: "Max" };
+        },
+        components: { "app-info": Info }
+    };
+</script>
+<style>
+    p {
+    font-style: italic;
+    }
+</style>
